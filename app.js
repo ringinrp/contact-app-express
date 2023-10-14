c7a2aca6918c112af8cefdc26607caf47cc5904e
ringinrp <ringinrp21@gmail.com>
2023-10-14 11:39:47 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts');
-const { loadContact, findContact } = require('./utils/contacts');
+const { loadContact, findContact, addContact } = require('./utils/contacts');
+const {body, validationResult, check} = require('express-validator');
 
 const app = express()
 const port = 3000
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 //built-in middleware 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}));
 
 
 app.get('/', (req, res) => {
@@ -53,6 +55,29 @@ app.get('/contact', (req, res) => {
         contacts,
     });
 })
+
+//halaman form tambah data contact
+app.get('/contact/add', (req, res) => {
+    res.render('add-contact', {
+        title: 'Form Tambah Data Contact',
+        layout: 'layouts/main-layout',
+    })
+})
+
+//proses data contact
+app.post('/contact', [
+    check('email', 'Email tidak valid!').isEmail(),
+    check('nohp', 'Nomer Handphone tidak valid!').isMobilePhone('id-ID') 
+], (req, res)=>{
+    const errors = validationResult (req);
+    if (!errors.isEmpty()){
+        return res.status(404).json({errors: errors.array()});
+    }
+    // addContact(req.body);
+    // res.redirect('/contact');
+})
+
+//halaman detail contact
 app.get('/contact/:nama', (req, res) => {
     const contact = findContact(req.params.nama);
 
