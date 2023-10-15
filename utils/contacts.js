187e4f2b3ce6f4cca86199ced0e88ed365224780
ringinrp <ringinrp21@gmail.com>
2023-10-15 15:59:27 +0700

    update

--- utils/contacts.js
+++ utils/contacts.js
@@ -39,4 +39,10 @@ const addContact = (contact) => {
     saveContacts(contacts);
 }
 
-module.exports = { loadContact, findContact, addContact };
+//cek nama yang duplikat
+const cekDuplikat = (nama) => {
+    const contacts = loadContact();
+    return contacts.find((contact)=>contact.nama === nama);
+}
+
+module.exports = { loadContact, findContact, addContact, cekDuplikat };
